45249c41442655503360e102d16de200042d0b14
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-25 15:13:46 +0100

    temp: removed dep

--- libs/api/auth/data-access/src/lib/services/auth/api-auth.service.ts
+++ libs/api/auth/data-access/src/lib/services/auth/api-auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class ApiAuthService  {
-  constructor(private jwt: JwtService) {}
+  constructor() {}
 }
