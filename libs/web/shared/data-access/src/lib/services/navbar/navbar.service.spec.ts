e22dcccd7140e9eac64413684b36bfecdfd667ed
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-27 16:49:19 +0100

    added navbar service tests

--- libs/web/shared/data-access/src/lib/services/navbar/navbar.service.spec.ts
+++ libs/web/shared/data-access/src/lib/services/navbar/navbar.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { filter, take, toArray } from 'rxjs';
 
 import { NavbarService } from './navbar.service';
 
-describe('NavbarService', () => {
+describe.only('NavbarService', () => {
   let service: NavbarService;
 
   beforeEach(() => {
@@ -12,5 +13,32 @@ describe('NavbarService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return latest layer', (done) => {
+    const title$ = service.latestProp$('title');
+
+    service.registerNavbarLayer({ title: 'layer1' });
+    service.registerNavbarLayer({ title: 'layer2' });
+
+    title$.subscribe((prop) => {
+      expect(prop?.value).toBe('layer2');
+      done();
+    });
+  });
+
+  it('should update the layer', (done) => {
+    const title$ = service.latestProp$('title').pipe(
+      filter((e) => e !== undefined),
+      take(2),
+      toArray(),
+    );
+    title$.subscribe((props) => {
+      expect(props.map((e) => e?.value)).toEqual(['title1', 'title2']);
+      done();
+    });
+
+    const layer = service.registerNavbarLayer({ title: 'title1' });
+    layer.update({ title: 'title2' });
   });
 });
