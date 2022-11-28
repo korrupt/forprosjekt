258f18a1eccd165f99d72be1db9d736a465886c9
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-28 15:43:29 +0100

    added apollo to web shell

--- libs/web/shell/feature/src/lib/web-shell.module.ts
+++ libs/web/shell/feature/src/lib/web-shell.module.ts
@@ -4,8 +4,27 @@ import { RouterModule } from '@angular/router';
 import { WEB_SHELL_ROUTES } from './web-shell.routes';
 import { WebLayoutShellModule } from '@forprosjekt/web/layout/feature/shell';
 
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(WEB_SHELL_ROUTES), WebLayoutShellModule],
+  imports: [CommonModule, RouterModule.forRoot(WEB_SHELL_ROUTES), WebLayoutShellModule, ApolloModule, HttpClientModule],
   exports: [RouterModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: async (link: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: link.create({
+            uri: '/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
 })
 export class WebShellModule {}
