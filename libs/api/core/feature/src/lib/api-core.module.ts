e66cfcf298b50da709415ad117d3a888dc1897b4
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-29 12:20:00 +0100

    added global validation pipe

--- libs/api/core/feature/src/lib/api-core.module.ts
+++ libs/api/core/feature/src/lib/api-core.module.ts
@@ -2,10 +2,16 @@ import { ApiApolloModule } from '@forprosjekt/api/apollo/feature';
 import { ApiAuthModule } from '@forprosjekt/api/auth/feature';
 import { ApiDatabaseModule } from '@forprosjekt/api/database/feature';
 import { ApiUserModule } from '@forprosjekt/api/user/feature';
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ApiDatabaseModule, ApiAuthModule, ApiUserModule, ApiApolloModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ transform: true, whitelist: true }),
+    },
+  ],
 })
 export class ApiCoreModule {}
