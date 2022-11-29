4b4a2a206f368ab4cd9ffd4c77becf62f0cfc5ee
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-29 11:08:16 +0100

    added loggedInUser path

--- libs/api/user/data-access/src/lib/resolvers/user.resolver.ts
+++ libs/api/user/data-access/src/lib/resolvers/user.resolver.ts
@@ -1,6 +1,6 @@
 import { ApolloGuard, AuthUser, GQLAuth } from '@forprosjekt/api/auth/utils';
 import { User } from '@forprosjekt/api/user/utils';
-import { UseGuards } from '@nestjs/common';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
 import { Args, ID, Query, Resolver } from '@nestjs/graphql';
 import { ApiUserService } from '../services';
 
@@ -17,5 +17,11 @@ export class UserResolver {
   @Query(() => User, { name: 'user' })
   public findUser(@GQLAuth() auth: AuthUser, @Args('userId', { type: () => ID }) userId: string) {
     return this.user.findUser(userId, true);
+  }
+
+  @Query(() => User, { name: 'me' })
+  public findLoggedInUser(@GQLAuth() auth: AuthUser) {
+    if (!auth.id) throw new ForbiddenException(`User not logged in.`);
+    return this.user.findUser(auth.id, true);
   }
 }
