01cdebc6aa078af9b2f71a01a71646de9daa580a
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-11-29 12:04:20 +0100

    added correct migration

--- libs/api/database/cli/src/lib/migrations/1669719832582-acl-refactor.ts
+++ libs/api/database/cli/src/lib/migrations/1669719832582-acl-refactor.ts
@@ -1,13 +1,14 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class aclRefactor1669718923173 implements MigrationInterface {
-  name = 'aclRefactor1669718923173';
+export class aclRefactor1669719832582 implements MigrationInterface {
+  name = 'aclRefactor1669719832582';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user" ADD "ownerId" character varying NOT NULL`);
     await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
     await queryRunner.query(`ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
-    await queryRunner.query(`ALTER TABLE "user" ADD "roles" "public"."AccessRole" NOT NULL`);
+    await queryRunner.query(`CREATE TYPE "public"."AccessRole" AS ENUM('ADMIN', 'USER', 'ANON')`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "roles" "public"."AccessRole" NOT NULL DEFAULT ''`);
     await queryRunner.query(`ALTER TABLE "user_auth" ADD "ownerId" character varying NOT NULL`);
     await queryRunner.query(`ALTER TABLE "user_auth" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
     await queryRunner.query(`ALTER TABLE "user_auth" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
@@ -18,6 +19,7 @@ export class aclRefactor1669718923173 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "user_auth" DROP COLUMN "createdAt"`);
     await queryRunner.query(`ALTER TABLE "user_auth" DROP COLUMN "ownerId"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "roles"`);
+    await queryRunner.query(`DROP TYPE "public"."AccessRole"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updatedAt"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdAt"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "ownerId"`);
