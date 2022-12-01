5124ddb142d2bdcbd29753add05bcc9e1d00e04d
Andreas Ipsen <k0rrupt.dev@gmail.com>
2022-12-01 12:12:34 +0100

    mem

--- libs/api/battery/data-access/src/lib/services/battery/api-battery.service.spec.ts
+++ libs/api/battery/data-access/src/lib/services/battery/api-battery.service.spec.ts
@@ -2,10 +2,12 @@ import { Battery, UserBattery } from '@forprosjekt/api/battery/utils';
 import { User } from '@forprosjekt/api/user/utils';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ApiBatteryService } from './api-battery.service';
 
 describe('ApiBatteryService', () => {
   let service: ApiBatteryService;
+  let user: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,6 +29,7 @@ describe('ApiBatteryService', () => {
     }).compile();
 
     service = module.get<ApiBatteryService>(ApiBatteryService);
+    user = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
